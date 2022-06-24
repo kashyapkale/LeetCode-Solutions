583a99b104d105872f44871aa751f37c86865abb
Kashyap Milind Kale <43263855+kashyapkale@users.noreply.github.com>
2022-06-25 01:22:59 +0530

    Time: 4 ms (55.83%), Space: 6.8 MB (30.87%) - LeetHub

---
+++ 67-add-binary/67-add-binary.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+//BinarySum
+pair<char, char> getSingleDigitSum(char A, char B){
+	
+    if (A == '1' && B == '1')
+        return make_pair('1', '0');
+    
+
+	if (A == '0' && B == '0')
+		return make_pair('0', '0');
+
+	return make_pair('0', '1');
+}
+
+void computeExtraDigits(string &ans, string &s, char carry, int remainingLength){
+	while (remainingLength--) {
+		pair<char, char> currSum = getSingleDigitSum(s[remainingLength - 1], carry);
+		carry = currSum.first;
+		ans += currSum.second;
+	}
+}
+
+void addExtraDigits(string &s, int remainingLength){
+	while (remainingLength--)
+		s = '0' + s;
+}
+
+
+
+string addBinary(string s1, string s2){
+	
+	char carry = '0';
+	string ans = "";
+	int remainingLength = max(s1.length(), s2.length()) - min(s1.length(), s2.length());
+
+	if (s1.length() > s2.length()) {
+		addExtraDigits(s2, remainingLength);
+	}
+	else if (s1.length() < s2.length()) {
+		addExtraDigits(s1, remainingLength);
+	}
+
+    int len = max(s1.length(), s2.length());
+	while (len) {
+		pair<char, char> currSum1 = getSingleDigitSum(s1[len - 1], carry);
+		char tempCarry = currSum1.first, tempSum = currSum1.second;
+		pair<char, char> currSum2 = getSingleDigitSum(tempSum, s2[len - 1]);
+		ans += currSum2.second;
+		carry = (currSum2.first == '1') ? '1':tempCarry;
+        len--;
+	}
+	
+
+	if (carry == '1')
+		ans += '1';
+
+	reverse(ans.begin(), ans.end());
+	return ans;
+}
+
+//    abcdef
+//defghklmnr
+
+
+
+
+
+};
