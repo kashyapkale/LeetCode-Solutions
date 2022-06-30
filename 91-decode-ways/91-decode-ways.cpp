ee43f41075c47ee796dc83994c310ee563dedac9
Kashyap Milind Kale <43263855+kashyapkale@users.noreply.github.com>
2022-06-30 23:12:26 +0530

    Time: 10 ms (10.42%), Space: 7.1 MB (20.70%) - LeetHub

---
+++ 91-decode-ways/91-decode-ways.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+int numDecodingsUtil(string s, int len, vector<int> &dp) {
+
+    if (dp[len] != -1)
+        return dp[len];
+
+    if (s[0] == '0')
+        return dp[len] = 0;
+
+    if (len == 0)
+        return dp[len] = 0;
+
+    if (len == 1)
+        return dp[len] = 1;
+
+    if (len == 2) {
+        if (s[0] != '0' && s[1] != '0')
+            return dp[len] = (stoi(s.substr(0, 2)) <= 26) ? 2 : 1;
+        else if ((s[0] == '1' || s[0] == '2') && s[1] == '0')
+            return dp[len] = 1;
+        else
+            return dp[len] = 0;
+    }
+
+
+    int singleDigit = numDecodingsUtil(s.substr(1, len - 1), len - 1,dp);
+    int doubleDigit = 0;
+
+    if (s.length() >= 2 && (stoi(s.substr(0, 2)) <= 26)) {
+        doubleDigit = numDecodingsUtil(s.substr(2, len - 2), len - 2,dp);
+    }
+
+    return dp[len] = singleDigit + doubleDigit;
+
+}
+int numDecodings(string s){
+    vector<int> dp(s.length() + 1, -1);
+
+    return numDecodingsUtil(s, s.length(), dp);
+}
+
+};
